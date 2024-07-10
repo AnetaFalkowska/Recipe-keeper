ee685da28f1ecbeb6be6b09227227e335094a517
Aneta Falkowska <aneta.falkowska@gmail.com>
2024-07-10 16:37:05 +0200

    Combine Header into MainNavigation component

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,22 +1,43 @@
 import { NavLink } from "react-router-dom";
-import classes from "./MainNavigation.module.css"
-import Header from "./Header";
-
+import classes from "./MainNavigation.module.css";
+import FoodImg from "../assets/ramen.png";
+import SearchBar from "./SearchBar";
 
 export default function MainNavigation() {
   return (
-    <>
-   
-      <div className={classes.menu}>
-        <nav>
-          <ul>
-            <NavLink to="/" className={({isActive})=> isActive ? classes.active : undefined} end>Home</NavLink>
-            <NavLink to="/recipes" className={({isActive})=> isActive ? classes.active : undefined}>Recipes</NavLink>
-          </ul>
-        </nav>
-        
-      </div>
-      <Header />
-    </>
+    <header>
+      <nav className={classes.menu}>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/recipes"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Recipes
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <section className={classes["search-section"]}>
+        <div className={classes.logo}>
+          <img src={FoodImg} alt="ramen picture" />
+          <h1>Recipe keeper</h1>
+        </div>
+        <SearchBar />
+      </section>
+    </header>
   );
 }
